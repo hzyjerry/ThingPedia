e225970608b3349c5da5d9f84bafd071e6e1126a
braincat <hselin.chen@gmail.com>
2015-04-24 10:10:18 -0700

    add rule browse

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var create = require('./routes/create');
+var browse = require('./routes/browse');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/create', create);
+app.use('/browse', browse);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
