b4272fc57708ffdad61537401a67da761504636b
braincat <hselin.chen@gmail.com>
2015-04-24 12:48:30 -0700

    added rule display html

--- public/js/browse.js
+++ public/js/browse.js
@@ -1,5 +1,28 @@
-function displayRule(element, index, array) {
+function displayRule(rule, index, array) {
+  //var aElement = document.createElement("a");
+  //aElement.className = 'list-group-item';
+  //aElement.setAttribute('href', 'http://google.com');
 
+  var ruleLink = $('<a/>', {
+    class: 'list-group-item',
+    href: 'http://google.com',
+  });
+
+  var ruleHeading = $('<h4/>', {
+    class: 'list-group-item-heading',
+    text: rule.name,
+  });
+
+  var ruleDescription = $('<p/>', {
+    class: 'list-group-item-text',
+    text: rule.description,
+  });
+
+
+  ruleLink.append(ruleHeading);
+  ruleLink.append(ruleDescription);
+
+  $('#rule-list').append(ruleLink);
 }
 
 $.ajax({
