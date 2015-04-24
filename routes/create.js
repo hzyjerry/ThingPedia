29402cff3aeffa4578e2418ed817a4f2187d1776
Giovanni Campagna <gcampagn@cs.stanford.edu>
2015-04-24 11:25:14 -0700

    create: add description of triggers and channels
    These are stored on the server and converted into HTML in the client,
    so that we can update the capability of the phone app without touching
    the HTML code

--- routes/create.js
+++ routes/create.js
@@ -3,7 +3,63 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('create', { title: 'Rulepedia - Create a new rule' });
+    res.render('create', { title: 'Rulepedia - Create a new rule' });
+});
+
+router.get('/triggers', function(req, res, next) {
+    // FIXME: unhardcode
+
+    res.json([
+        { id: 'facebook', description: "Facebook",
+          events: [
+              { id: 'message-received',
+                description: "When I receive a message",
+                params: [
+                    { id: 'content-contains',
+                      type: 'textarea',
+                      optional: true,
+                      description: "containing this text" },
+                    { id: 'sender-matches',
+                      type: 'facebook-contact',
+                      optional: true,
+                      description: "from this person" }
+                ]
+              },
+              { id: 'photo-tagged',
+                description: "When I\'m tagged on a picture" },
+              { id: 'starred-contact-notification',
+                description: "When a friend does something",
+                params: [
+                    { id: 'contact-matches',
+                      type: 'facebook-contact',
+                      optional: true,
+                      description: "and the friend is" }
+                ] }
+          ] },
+        { id: 'weather', description: "Weather",
+          events: [
+              { id: 'given-forecast',
+                description: "When the forecast says",
+                params: [
+                    { id: 'forecast',
+                      type: 'select',
+                      options: { 'rain': "Rain", 'sun': "Sunny" },
+                    }]
+              },
+              { id: 'given-temperature',
+                description: "When the temperature is",
+                params: [
+                    { id: 'comparator',
+                      type: 'select',
+                      options: { 'gt': "Above", 'lt': "Below" },
+                    },
+                    { id: 'value',
+                      type: 'temperature',
+                    }
+                ]
+              }
+          ] }
+    ]);
 });
 
 module.exports = router;
