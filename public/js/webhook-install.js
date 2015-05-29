cfb1d4aca56e7d22253deafcdad692bd92854579
Giovanni Campagna <gcampagn@cs.stanford.edu>
2015-05-29 11:34:30 -0700

    Moar fixes

--- public/js/webhook-install.js
+++ public/js/webhook-install.js
@@ -14,7 +14,15 @@
       ddx.send("GetSubscriptionPublishURL", req, function(resp) {
         var url = "https://vast-hamlet-6003.herokuapp.com/webhook/hook/" + Base64.encodeURI(resp.HookURL);
         $('#retry').prop('href', url);
-        document.location.href = url;
+        Omlet.exit(Omlet.createRDL({
+    	    noun: "app",
+    	    displayTitle: "Sabrina",
+    	    displayThumbnailUrl: "https://vast-hamlet-6003.herokuapp.com/images/world3.jpg",
+    	    displayText: "Complete installation",
+    	    json: false,
+    	    webCallback: url,
+    	    callback: url,
+        }));
       }, function(){});
     });
 })()
